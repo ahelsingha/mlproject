ce68599bb365be5529a07d7321f05327d1ec205c
ahelsingha <singhaahel545@gmail.com>
2025-10-26 21:15:49 +0530

    Data Ingestion

---
+++ src/components/data_transformation.py
@@ -0,0 +1,20 @@
+import sys
+from dataclasses import dataclass
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+
+from src.exception import CustomException
+from src.logger import logging
+
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+
